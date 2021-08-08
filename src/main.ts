aeefe292905b2546b7ac5a7ac376d439754e4de5
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-08-08 19:46:30 +0700

    refactor(rename variable): rename variable timeline to catTimelineY

--- src/main.ts
+++ src/main.ts
@@ -11,13 +11,13 @@ let audio = document.createElement('audio');
 audio.src = "/music.mp3"
 
 // GSAP animation timeline
-const timeline = gsap.timeline({paused: true})
+const catTimelineY = gsap.timeline({paused: true})
 const rainbowTimeline = gsap.timeline({paused: true})
 const infinityConfig = {repeat: -1, yoyo: true}
 rainbowTimeline.to('#rainbow', {width: '100vw', duration: 5, delay: 5})
-timeline.to('#wrapper', {y: 100, ...infinityConfig})
-timeline.to('#wrapper', {y: -100, ...infinityConfig})
-timeline.to('#cat-container', {
+catTimelineY.to('#wrapper', {y: 100, ...infinityConfig})
+catTimelineY.to('#wrapper', {y: -100, ...infinityConfig})
+catTimelineY.to('#cat-container', {
     backgroundPositionX: -2000, repeat: -1, ease: 'linear', duration: 3, delay: 3
 })
 
@@ -25,14 +25,14 @@ timeline.to('#cat-container', {
 if (playBtn && pauseBtn) {
     playBtn.addEventListener('click', () => {
         audio.play().then(() => {
-            timeline.play()
+            catTimelineY.play()
             rainbowTimeline.play()
         })
     })
 
     pauseBtn.addEventListener('click', () => {
         audio.pause()
-        timeline.pause()
+        catTimelineY.pause()
         rainbowTimeline.pause()
     })
 }
